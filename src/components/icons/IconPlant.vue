d0ad2399f18bbafa916b9569e8f1721b5d5702b5
Maceo <basse.maceo@gmail.com>
2022-11-07 09:46:20 +0100

    MB add Icon

---
+++ src/components/icons/IconPlant.vue
@@ -0,0 +1,15 @@
+<template>
+  <svg width="1em" height="1em" viewBox="0 0 15 15">
+    <path
+      fill="none"
+      stroke="currentColor"
+      d="M7.5 15V7m0 .5v3m0-3a4 4 0 0 0-4-4h-3v3a4 4 0 0 0 4 4h3m0-3h3a4 4 0 0 0 4-4v-3h-3a4 4 0 0 0-4 4v3Zm0 0l4-4m-4 7l-4-4"
+    ></path>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "TeenyiconsPlantOutline",
+};
+</script>
---
+++ src/components/icons/IconReturn.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg width="1em" height="1em" viewBox="0 0 24 24">
+    <g
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+    >
+      <circle cx="7" cy="17" r="2"></circle>
+      <circle cx="17" cy="17" r="2"></circle>
+      <path
+        d="M5 17H3V6a1 1 0 0 1 1-1h9v6H8l2 2m0-4l-2 2m1 6h6M13 6h5l3 5v6h-2"
+      ></path>
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "TablerTruckReturn",
+};
+</script>

6bc3f60e3049d5f0033e6c14bb1fcb8a5ac7fa19
Maceo <basse.maceo@gmail.com>
2022-11-07 10:56:37 +0100

    Add header and footer component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import HomeComponent from "../components/HomeComponent.vue";
+import Header from "../components/HeaderComponent.vue";
+import Footer from "../components/FooterComponent.vue";
 </script>
 
 <template>
-  <main>
+  <div id="app">
+    <Header />
     <HomeComponent />
-  </main>
+    <Footer />
+  </div>
 </template>

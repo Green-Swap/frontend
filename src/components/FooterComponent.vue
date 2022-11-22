6e9c4829f257973cbabca8c5c20ecb8bec54b7d1
Maceo <basse.maceo@gmail.com>
2022-11-22 12:01:12 +0100

    add footer

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="pg-footer">
+    <footer class="footer">
+      <div class="footer-content">
+        <div class="footer-content-column">
+          <div class="footer-menu">
+            <h2 class="footer-menu-name">Help</h2>
+            <ul id="menu-get-started" class="footer-menu-list">
+              <li
+                class="menu-item menu-item-type-post_type menu-item-object-product"
+              >
+                <a href="#">Shop At Little Garde</a>
+              </li>
+              <li
+                class="menu-item menu-item-type-post_type menu-item-object-product"
+              >
+                <a href="#">Product</a>
+              </li>
+              <li
+                class="menu-item menu-item-type-post_type menu-item-object-product"
+              >
+                <a href="#">Gift Card</a>
+              </li>
+              <li
+                class="menu-item menu-item-type-post_type menu-item-object-product"
+              >
+                <a href="#">Gift Receipt</a>
+              </li>
+
+              <li
+                class="menu-item menu-item-type-post_type menu-item-object-product"
+              >
+                <a href="#">Shipping</a>
+              </li>
+              <li
+                class="menu-item menu-item-type-post_type menu-item-object-product"
+              >
+                <a href="#">My Account</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="footer-content-column">
+          <div class="footer-menu">
+            <h2 class="footer-menu-name">Company</h2>
+            <ul id="menu-company" class="footer-menu-list">
+              <li
+                class="menu-item menu-item-type-post_type menu-item-object-page"
+              >
+                <a href="#">Contact</a>
+              </li>
+              <li
+                class="menu-item menu-item-type-taxonomy menu-item-object-category"
+              >
+                <a href="#">News</a>
+              </li>
+              <li
+                class="menu-item menu-item-type-post_type menu-item-object-page"
+              >
+                <a href="#">Careers</a>
+              </li>
+              <li
+                class="menu-item menu-item-type-post_type menu-item-object-product"
+              >
+                <a href="#">Exchanges And Returns</a>
+              </li>
+              <li
+                class="menu-item menu-item-type-post_type menu-item-object-product"
+              >
+                <a href="#">Shops And Company</a>
+              </li>
+              <li
+                class="menu-item menu-item-type-post_type menu-item-object-product"
+              >
+                <a href="#">My Account</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="footer-content-column">
+          <div class="footer-menu">
+            <h2 class="footer-menu-name">Quick Links</h2>
+            <ul id="menu-quick-links" class="footer-menu-list">
+              <li
+                class="menu-item menu-item-type-custom menu-item-object-custom"
+              >
+                <a target="_blank" rel="noopener noreferrer" href="#"
+                  >Support Center</a
+                >
+              </li>
+              <li
+                class="menu-item menu-item-type-custom menu-item-object-custom"
+              >
+                <a target="_blank" rel="noopener noreferrer" href="#"
+                  >Service Status</a
+                >
+              </li>
+              <li
+                class="menu-item menu-item-type-post_type menu-item-object-page"
+              >
+                <a href="#">Security</a>
+              </li>
+              <li
+                class="menu-item menu-item-type-post_type menu-item-object-page"
+              >
+                <a href="#">Blog</a>
+              </li>
+              <li
+                class="menu-item menu-item-type-post_type_archive menu-item-object-customer"
+              >
+                <a href="#">Customers</a>
+              </li>
+              <li
+                class="menu-item menu-item-type-post_type menu-item-object-page"
+              >
+                <a href="#">Reviews</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="footer-copyright">
+        <div class="footer-copyright-wrapper">
+          <p class="footer-copyright-text">
+            <a class="footer-copyright-link" href="#" target="_self">
+              ©2022 | Designed By: Alexandre Le Hir | All rights reserved.
+            </a>
+          </p>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<style lang="scss">
+@import "@/assets/scss/footer.scss";
+</style>

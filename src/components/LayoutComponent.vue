26e84ba1354012349b3b6ed0cb5f848a12a2d011
Philippe VILARINHO <59869747+tifiloutube@users.noreply.github.com>
2022-11-24 14:14:56 +0100

    Commencement du layout
    Mise en place de la structure du layout dans le fichier LayoutComponent et mise en place du style de celui-ci.
    Rectification d'un bug au niveau du header.

---
+++ src/components/LayoutComponent.vue
@@ -0,0 +1,34 @@
+<style>
+  @import "../assets/scss/layout.scss";
+</style>
+
+<template>
+  <main>
+    <section>
+      <aside>
+        <img :src="require('../assets/images/layoutImg.svg')" alt="photo de plantes dans un salon">
+      </aside>
+      <article>
+        <div>
+          <h1>Beyond <br> Nature</h1>
+          <p>Envie d’en savoir plus sur Little Garden ? Découvre notre histoire.</p>
+          <button>SHOP NOW</button>
+        </div>
+        <div>
+          <div>
+            <button>
+              <img :src="require('../assets/images/ctrl-left.svg')" alt="flèche directionnel vers la gauche">
+            </button>
+            <button>
+              <img :src="require('../assets/images/ctrl-right.svg')" alt="flèche directionnel vers la droite">
+            </button>
+          </div>
+          <div>
+            <h3>the new lookbook</h3>
+            <p>Daily Inspiration and Beauty Advices.</p>
+          </div>
+        </div>
+      </article>
+    </section>
+  </main>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import HomeComponent from "../components/HomeComponent.vue";
 import Header from "../components/HeaderComponent.vue";
+import Layout from "../components/LayoutComponent.vue";
 import Footer from "../components/FooterComponent.vue";
 </script>
 
 <template>
   <div id="app">
     <Header />
-    <HomeComponent />
+    <Layout />
     <Footer />
   </div>
 </template>

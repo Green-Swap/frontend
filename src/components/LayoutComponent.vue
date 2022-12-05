bd111e2cea1523ff16746755a8ff1c23fc0350a5
Philippe VILARINHO <59869747+tifiloutube@users.noreply.github.com>
2022-12-05 16:57:59 +0100

    Layout
    Avancement du layout parti top.

--- src/components/LayoutComponent.vue
+++ src/components/LayoutComponent.vue
@@ -8,24 +8,37 @@
       <aside>
         <img :src="require('../assets/images/layoutImg.svg')" alt="photo de plantes dans un salon">
       </aside>
-      <article>
+      <article class="first_article">
         <div>
-          <h1>Beyond <br> Nature</h1>
+          <h1>Beyond<br> Nature</h1>
           <p>Envie d’en savoir plus sur Little Garden ? Découvre notre histoire.</p>
           <button>SHOP NOW</button>
         </div>
-        <div>
+      </article>
+      <div class="social_network">
+        <span></span>
+        <p>PV</p>
+        <p>AD</p>
+        <p>MB</p>
+        <p>CT</p>
+        <p>NJ</p>
+        <span></span>
+      </div>
+      <article class="second_article">
+        <div class="left_block">
+          <button>
+            <img :src="require('../assets/images/ctrl-left.svg')" alt="flèche vers la gauche">
+          </button>
+          <button>
+            <img :src="require('../assets/images/ctrl-right.svg')" alt="flèche vers la droite" />
+          </button>
+        </div>
+        <div class="right_block">
           <div>
-            <button>
-              <img :src="require('../assets/images/ctrl-left.svg')" alt="flèche directionnel vers la gauche">
-            </button>
-            <button>
-              <img :src="require('../assets/images/ctrl-right.svg')" alt="flèche directionnel vers la droite">
-            </button>
-          </div>
-          <div>
-            <h3>the new lookbook</h3>
-            <p>Daily Inspiration and Beauty Advices.</p>
+            <h3>THE NEW LOOKBOOK</h3>
+            <p>
+              Daily Inspiration and Beauty <br> Advices.
+            </p>
           </div>
         </div>
       </article>

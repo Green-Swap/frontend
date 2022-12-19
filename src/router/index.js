f870c46d2f11ab91c08b2c223e9789153dbb706f
Maceo <basse.maceo@gmail.com>
2022-12-19 12:08:05 +0100

    start contact page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import Contact from "../views/ContactView.vue";
 
 Vue.use(VueRouter);
 
@@ -12,6 +13,11 @@ const router = new VueRouter({
       path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/contact",
+      name: "contact",
+      component: Contact,
     },
   ],
 });
